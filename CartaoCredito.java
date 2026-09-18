70496f14626cac3f1db455472d4cf8c573f76942
renanb12 <renanbernardo315@gmail.com>
2026-09-18 16:04:01 -0300

    Metodos calcularTaxa, enviarNotificacao e estornar

--- CartaoCredito.java
+++ CartaoCredito.java
@@ -16,17 +16,18 @@ public class CartaoCredito extends Pagamento implements Estornavel {
     // TODO: taxa de 3,5% sobre o valor, mais R$ 0,50 por parcela além da 1ª
     @Override
     public double calcularTaxa() {
-        return 0.0;
+        return (getValor() * 0.035) + (0.5 * (parcelas - 1));
     }
 
     // TODO: imprima uma mensagem informando o número de parcelas
     @Override
     public void enviarNotificacao() {
+        System.out.println("O quantidade de parcelas é: " + parcelas);
     }
 
     // TODO: cartão de crédito sempre pode ser estornado — retorne true
     @Override
     public boolean estornar() {
-        return false;
+        return true;
     }
 }
