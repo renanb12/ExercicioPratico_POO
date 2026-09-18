c1dccc1cefeb3b5d2530f686bc629d61a68d0363
renanb12 <renanbernardo315@gmail.com>
2026-09-18 17:18:00 -0300

    Implementado a Interface Rastreavel e ja adicionado na Classe Boleto

--- Boleto.java
+++ Boleto.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 // Repare: Boleto NÃO implementa Estornavel — depois de compensado, não há como estornar.
 // Isso é proposital: mostra que interfaces são contratos opcionais por subclasse,
 // diferente de um método herdado da classe abstrata (que toda subclasse carrega).
-public class Boleto extends Pagamento {
+public class Boleto extends Pagamento implements Rastreavel {
 
     public Boleto(String idTransacao, double valor, LocalDate data) {
         super(idTransacao, valor, data);
@@ -13,6 +13,11 @@ public class Boleto extends Pagamento {
     @Override
     public double calcularTaxa() {
         return 3.49;
+    }
+
+    @Override
+    public String codigoRastreio() {
+        return "Codigo: \n00191.57379 00391.944279 44354.919928 6 26210000352155";
     }
 
     // TODO: imprima uma mensagem confirmando que o boleto foi gerado
---
+++ Rastreavel.java
@@ -0,0 +1,3 @@
+public interface Rastreavel {
+    String codigoRastreio();
+}
